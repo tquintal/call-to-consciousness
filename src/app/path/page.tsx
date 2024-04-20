89339cbae903f34aa812c62c491de61e5b6bb30d
Tomás Quintal <16324911+tquintal@users.noreply.github.com>
2024-04-20 16:28:10 +0100

    logic update at path page

--- src/app/path/page.tsx
+++ src/app/path/page.tsx
@@ -1,15 +1,6 @@
 import Link from "next/link";
 import data from "../data/data.json";
 
-export default function Contact() {
-  return (
-    <div className="flex flex-col pb-8 bg-[#F3F3F3]">
-      <div className="w-full h-44 bg-slate-300" />
-      <Path content={data} />
-    </div>
-  );
-}
-
 type ContentType = {
   pathTitle: string;
   items: {
@@ -21,40 +12,45 @@ type ContentType = {
   }[];
 };
 
-const Path = ({ content }: { content: ContentType[] }) => {
+export default function Contact() {
+  const content: ContentType[] = data;
+
   return (
-    <div className="flex flex-col gap-8 2xl:pr-52 2xl:pl-52 p-8">
-      {content.map((el, i) => (
-        <div key={i} className="flex flex-col gap-8">
-          <div className="grid grid-flow-row xl:grid-flow-col xl:grid-cols-8">
-            <h1 className="font-semibold text-2xl xl:col-span-2">{el.pathTitle}</h1>
-            <div className="xl:col-span-6">
-              {el.items.map((item, i) => (
-                <div key={i} className="pt-4 grid grid-flow-row xl:grid-flow-col xl:grid-cols-8 xl:pt-0">
-                  <span className="xl:col-span-1 font-light">{item.date}</span>
-                  <div className="xl:col-span-7">
-                    <h2 className="font-semibold text-xl">{item.title}</h2>
-                    {item.link && (
-                      <Link href={item.link} className="font-semibold">
-                        {item.subtitle}
-                      </Link>
-                    )}
-                    {!item.link && <h3 className="font-semibold">{item.subtitle}</h3>}
-                    {item.content && (
-                      <ul>
-                        {item.content.map((el, i) => (
-                          <li key={i}>- {el}</li>
-                        ))}
-                      </ul>
-                    )}
+    <div className="flex flex-col pb-8 bg-[#F3F3F3]">
+      <div className="w-full h-44 bg-slate-300" />
+      <div className="flex flex-col gap-8 2xl:pr-52 2xl:pl-52 p-8">
+        {content.map((el, i) => (
+          <div key={i} className="flex flex-col gap-8">
+            <div className="grid grid-flow-row xl:grid-flow-col xl:grid-cols-8">
+              <h1 className="font-semibold text-2xl xl:col-span-2">{el.pathTitle}</h1>
+              <div className="xl:col-span-6">
+                {el.items.map((item, i) => (
+                  <div key={i} className="pt-4 grid grid-flow-row xl:grid-flow-col xl:grid-cols-8 xl:pt-0">
+                    <span className="xl:col-span-1 font-light">{item.date}</span>
+                    <div className="xl:col-span-7">
+                      <h2 className="font-semibold text-xl">{item.title}</h2>
+                      {item.link && (
+                        <Link href={item.link} className="font-semibold">
+                          {item.subtitle}
+                        </Link>
+                      )}
+                      {!item.link && <h3 className="font-semibold">{item.subtitle}</h3>}
+                      {item.content && (
+                        <ul>
+                          {item.content.map((el, i) => (
+                            <li key={i}>- {el}</li>
+                          ))}
+                        </ul>
+                      )}
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
+            {i < el.items.length && <div className="border border-b-0 border-zinc-400 w-full" />}
           </div>
-          {i < el.items.length && <div className="border border-b-0 border-zinc-400 w-full" />}
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
-};
+}
