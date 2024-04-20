fd629f0c724c61980370cec981a69500cc3b9746
Tomás Quintal <16324911+tquintal@users.noreply.github.com>
2024-04-20 16:08:56 +0100

    readme update

--- src/app/path/page.tsx
+++ src/app/path/page.tsx
@@ -10,10 +10,6 @@ export default function Contact() {
   );
 }
 
-{
-  /* <div className="border border-zinc-500 w-full" /> */
-}
-
 type ContentType = {
   pathTitle: string;
   items: {
@@ -23,56 +19,42 @@ type ContentType = {
     link?: string;
     content: string[];
   }[];
-}[];
+};
 
-const Path = ({ content }: { content: ContentType }) => {
+const Path = ({ content }: { content: ContentType[] }) => {
   return (
     <div className="flex flex-col gap-8 2xl:pr-52 2xl:pl-52 p-8">
       {content.map((el, i) => (
-        <div key={i} className="grid grid-flow-row xl:grid-flow-col xl:grid-cols-8">
-          <h1 className="font-semibold text-2xl xl:col-span-2">{el.pathTitle}</h1>
-          <div className="xl:col-span-6">
-            {el.items.map((item, i) => (
-              <div key={i} className="pt-4 grid grid-flow-row xl:grid-flow-col xl:grid-cols-8 xl:pt-0">
-                <span className="xl:col-span-1 font-light">{item.date}</span>
-                <div className="xl:col-span-7">
-                  <h2 className="font-semibold text-xl">{item.title}</h2>
-                  {item.link && (
-                    <Link href={item.link} className="font-semibold">
-                      {item.subtitle}
-                    </Link>
-                  )}
-                  {!item.link && <h3 className="font-semibold">{item.subtitle}</h3>}
-                  {item.content && (
-                    <ul>
-                      {item.content.map((el, i) => (
-                        <li key={i}>- {el}</li>
-                      ))}
-                    </ul>
-                  )}
+        <div key={i} className="flex flex-col gap-8">
+          <div className="grid grid-flow-row xl:grid-flow-col xl:grid-cols-8">
+            <h1 className="font-semibold text-2xl xl:col-span-2">{el.pathTitle}</h1>
+            <div className="xl:col-span-6">
+              {el.items.map((item, i) => (
+                <div key={i} className="pt-4 grid grid-flow-row xl:grid-flow-col xl:grid-cols-8 xl:pt-0">
+                  <span className="xl:col-span-1 font-light">{item.date}</span>
+                  <div className="xl:col-span-7">
+                    <h2 className="font-semibold text-xl">{item.title}</h2>
+                    {item.link && (
+                      <Link href={item.link} className="font-semibold">
+                        {item.subtitle}
+                      </Link>
+                    )}
+                    {!item.link && <h3 className="font-semibold">{item.subtitle}</h3>}
+                    {item.content && (
+                      <ul>
+                        {item.content.map((el, i) => (
+                          <li key={i}>- {el}</li>
+                        ))}
+                      </ul>
+                    )}
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
+          {i < el.items.length && <div className="border border-b-0 border-zinc-400 w-full" />}
         </div>
       ))}
-      {/* <p>{date}</p>
-      <div className="flex flex-col gap-4">
-        <h2 className="font-semibold text-xl">{title}</h2>
-        {link && (
-          <Link href={link} className="font-semibold">
-            {subtitle}
-          </Link>
-        )}
-        {!link && <h3 className="font-semibold">{subtitle}</h3>}
-        {content && (
-          <ul>
-            {content.map((el, i) => (
-              <li key={i}>- {el}</li>
-            ))}
-          </ul>
-        )}
-      </div> */}
     </div>
   );
 };
