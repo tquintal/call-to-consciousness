413b97e0a1d09f86db46a763b55b18aebbbb1f77
Tomás Quintal <16324911+tquintal@users.noreply.github.com>
2024-04-28 16:34:44 +0100

    homepage: completed with static content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import Image from "next/image";
+import call_to_consciousness from "../../public/call-to-consciousness.png";
+import avatar from "../../public/avatar.jpg";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="p-8 bg-[#E3E1DF] pt-36 flex gap-8 justify-center lg:items-center max-sm:items-center items-start lg:flex-row flex-col">
-      <div className="rounded-full w-full max-sm:pb-[100%] sm:h-96 sm:w-96 bg-zinc-300" />
+      <Image src={call_to_consciousness} alt="Call to consciousness" className="rounded-full sm:h-96 sm:w-96" />
       <div className="lg:max-w-[420px] flex flex-col gap-8">
         <div className="flex items-center justify-between">
-          <h1 className="text-8xl font-bold max-sm:w-full text-center sm:text-left">Olá!</h1>
-          <div className="sm:h-32 sm:w-32 h-24 w-24 bg-zinc-300 rounded-full max-sm:hidden" />
+          <h1 className="text-8xl font-bold max-sm:w-full">Olá!</h1>
+          <Image src={avatar} alt="Avatar" className="sm:h-32 sm:w-32 h-24 w-24 rounded-full max-sm:hidden object-cover" />
         </div>
         <div className="flex flex-col gap-4">
           <h2 className="font-semibold text-xl -mb-4">Sobre mim</h2>
@@ -22,17 +27,26 @@ export default function Home() {
           </p>
         </div>
         <div className="flex gap-4 sm:text-lg text-sm">
-          <div className="sm:h-32 sm:w-32 h-24 w-24 bg-zinc-300 rounded-full flex items-center justify-center font-semibold">
+          <Link
+            href="/path"
+            className="sm:h-32 sm:w-32 h-24 w-24 rounded-full flex items-center justify-center font-semibold bg-[#F3E1BA] border border-[#D6BE91] hover:bg-white hover:border-black transition-all duration-500"
+          >
             Percurso
-          </div>
-          <div className="sm:h-32 sm:w-32 h-24 w-24 bg-zinc-300 rounded-full flex items-center justify-center font-semibold">
+          </Link>
+          <Link
+            href="/services"
+            className="sm:h-32 sm:w-32 h-24 w-24 rounded-full flex items-center justify-center font-semibold cursor-pointer bg-[#BFA898] border border-[#A69275] hover:bg-white hover:border-black transition-all duration-500"
+          >
             Serviços
-          </div>
-          <div className="sm:h-32 sm:w-32 h-24 w-24 bg-zinc-300 rounded-full flex items-center justify-center font-semibold">
+          </Link>
+          <Link
+            href="/contact"
+            className="sm:h-32 sm:w-32 h-24 w-24 rounded-full flex items-center justify-center font-semibold cursor-pointer bg-[#D0E0DE] border border-[#ADCFCD] hover:bg-white hover:border-black transition-all duration-500"
+          >
             Contacto
-          </div>
+          </Link>
         </div>
-        <div className="rounded-full bg-zinc-300 w-72 h-72 sm:hidden" />
+        <Image src={avatar} alt="Avatar" className="rounded-full w-72 h-72 sm:hidden object-cover" />
       </div>
     </div>
   );
