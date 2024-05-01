a084368c20241a8bcfd39417113e2aa0739e889d
Tomás Quintal <16324911+tquintal@users.noreply.github.com>
2024-05-01 22:29:31 +0100

    small tweak

--- src/app/api/submit-form.ts
+++ src/app/api/submit-form.ts
@@ -13,10 +13,9 @@ export default async function sendEmail({ data }: { data?: ContactFormType }) {
       to: process.env.SMTP_USER,
       subject: data.subject,
       text: `
-      Nova mensagem de: ${data.email}
+      Nova mensagem de: ${data.email} em call-to-consciousness
       ${data.fname ? `Nome: ${data.fname} ${data.lname}` : ""}
-      ${data.phone ? `Telefone: ${data.phone}` : ""}
-      
+      ${data.phone ? `Telefone: ${data.phone}` : ""}      
       Mensagem: 
       ${data.message}
       `,
