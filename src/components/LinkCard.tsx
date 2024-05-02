3687d9b7c3e076192ff07e25bcc1e2898544093f
Tomás Quintal <16324911+tquintal@users.noreply.github.com>
2024-05-03 00:26:37 +0100

    small tweak

--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -11,7 +11,7 @@ export const LinkCard = ({ src, description, link }: Portfolio) => {
   return (
     <>
       {isLoading && <div className="w-96 h-52 shadow-lg bg-gray-300 duration-75 animate-pulse" />}
-      <CustomLink link={link ?? "#"}>
+      <CustomLink link={link}>
         <div className={`relative group h-fit shadow-lg ${link ? "cursor-pointer" : "cursor-default"}`}>
           <Image
             src={src}
@@ -25,7 +25,7 @@ export const LinkCard = ({ src, description, link }: Portfolio) => {
             <span className="text-white text-lg font-bold">{description}</span>
           </div>
         </div>
-        <span className="md:hidden font-light">- {description}</span>
+        <span className="font-light underline md:hidden ">- {description}</span>
       </CustomLink>
     </>
   );
