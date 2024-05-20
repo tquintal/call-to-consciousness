a1247e9062898154d3a22046b8d3d9720dbe9f30
Tomás Quintal <16324911+tquintal@users.noreply.github.com>
2024-05-21 00:52:57 +0100

    started implementing CRUD logic at path

--- src/server/api/routers/path.ts
+++ src/server/api/routers/path.ts
@@ -1,17 +1,15 @@
 import { z } from "zod";
+
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 
-const PathItem = z.object({
-  id: z.number(),
+export const PathItem = z.object({
   date: z.string().nullable(),
   title: z.string().nullable(),
   subTitle: z.string().nullable(),
-  content: z.array(z.string()),
-  pathId: z.number().nullable(),
+  content: z.string(),
 });
 
-const PathSchema = z.object({
-  id: z.number(),
+export const PathSchema = z.object({
   pathTitle: z.string(),
   items: z.array(PathItem),
 });
@@ -23,5 +21,57 @@ export const pathRouter = createTRPCRouter({
         items: true,
       },
     });
+  }),
+
+  add: publicProcedure.input(PathSchema).mutation(async ({ ctx, input }) => {
+    const { pathTitle, items } = input;
+    const newPath = await ctx.db.path.create({
+      data: {
+        pathTitle,
+        items: {
+          create: items,
+        },
+      },
+    });
+    return newPath;
+  }),
+
+  update: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        data: PathSchema,
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id, data } = input;
+      const { pathTitle, items } = data;
+      const updatedPath = await ctx.db.path.update({
+        where: { id },
+        data: {
+          pathTitle,
+          items: {
+            deleteMany: {},
+            create: items,
+          },
+        },
+      });
+      return updatedPath;
+    }),
+
+  delete: publicProcedure.input(z.object({ id: z.number() })).mutation(async ({ ctx, input }) => {
+    const { id } = input;
+    await ctx.db.path.delete({
+      where: { id },
+    });
+    return { success: true };
+  }),
+
+  deletePathItem: publicProcedure.input(z.object({ id: z.number() })).mutation(async ({ ctx, input }) => {
+    const { id } = input;
+    await ctx.db.pathItem.delete({
+      where: { id },
+    });
+    return { success: true };
   }),
 });
