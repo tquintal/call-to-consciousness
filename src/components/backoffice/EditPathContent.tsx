b3d8bc10c4386d6f4864233cf3258489d911cc8c
Tomás Quintal <16324911+tquintal@users.noreply.github.com>
2024-06-07 00:46:34 +0100

    path backoffice: confirmation modals implemented

--- src/components/backoffice/EditPathContent.tsx
+++ src/components/backoffice/EditPathContent.tsx
@@ -6,8 +6,12 @@ import { useViewModeContext } from "@/context/PreviewMode";
 import { PathFormSchemaType, PathSchemaType } from "@/types/Path";
 
 import { Button, Divider, Input, TextArea } from "../Elements";
+import { ConfirmationModal } from "../Modal";
+import { useState } from "react";
 
 export const EditPathContent = ({ data }: { data: PathSchemaType[] }) => {
+  const [isConfirmCancelOpen, setIsConfirmCancelOpen] = useState(false);
+  const [isConfirmSaveOpen, setIsConfirmSaveOpen] = useState(false);
   const { setIsViewMode } = useViewModeContext();
   const { updatePath } = usePath();
   const { control, handleSubmit, register } = useForm<PathFormSchemaType>({
@@ -24,60 +28,91 @@ export const EditPathContent = ({ data }: { data: PathSchemaType[] }) => {
   });
 
   return (
-    <form onSubmit={handleSubmit((data: PathFormSchemaType) => updatePath(data))}>
-      <div className="w-full h-44 bg-slate-300" />
-      <div className="flex flex-col gap-4 2xl:pr-52 2xl:pl-52 p-8">
-        {pathFields.map((path, pathIndex) => (
-          <div key={path.id} className="flex flex-col gap-4">
-            <div className="grid grid-flow-row xl:grid-flow-col xl:grid-cols-10 gap-4">
-              <div className="flex gap-1 xl:col-span-3">
-                <button
-                  type="button"
-                  onClick={() => removePath(pathIndex)}
-                  className="border p-3 h-fit bg-white border-black text-red-500"
-                >
-                  <IoTrashBinOutline />
-                </button>
-                <Input required {...register(`paths.${pathIndex}.pathTitle`)} placeholder="Título do percurso" />
+    <>
+      <form>
+        <div className="w-full h-44 bg-slate-300" />
+        <div className="flex flex-col gap-4 2xl:pr-52 2xl:pl-52 p-8">
+          {pathFields.map((path, pathIndex) => (
+            <div key={path.id} className="flex flex-col gap-4">
+              <div className="grid grid-flow-row xl:grid-flow-col xl:grid-cols-10 gap-4">
+                <div className="flex gap-1 xl:col-span-3">
+                  <button
+                    type="button"
+                    onClick={() => removePath(pathIndex)}
+                    className="border p-3 h-fit bg-white border-black text-red-500"
+                  >
+                    <IoTrashBinOutline />
+                  </button>
+                  <Input required {...register(`paths.${pathIndex}.pathTitle`)} placeholder="Título do percurso" />
+                </div>
+                <div className="xl:col-span-7 flex flex-col gap-4">
+                  <NestedItems nestIndex={pathIndex} control={control} register={register} />
+                </div>
               </div>
-              <div className="xl:col-span-7 flex flex-col gap-4">
-                <NestedItems nestIndex={pathIndex} control={control} register={register} />
-              </div>
+              {pathIndex + 1 <= pathFields.length && <Divider />}
             </div>
-            {pathIndex + 1 <= pathFields.length && <Divider />}
+          ))}
+          <button
+            type="button"
+            onClick={() =>
+              appendPath({
+                pathTitle: "",
+                items: [
+                  {
+                    id: null,
+                    pathId: null,
+                    content: "",
+                    date: "",
+                    title: "",
+                    titleLink: "",
+                    subTitle: "",
+                    subTitleLink: "",
+                  },
+                ],
+              })
+            }
+            className="self-end border p-3 bg-white border-black text-green-500"
+          >
+            Novo percurso
+          </button>
+          <div className="flex justify-between max-sm:border-t max-sm:border-black max-sm:p-3 sm:justify-end gap-2 max-sm:bg-white w-full max-sm:left-0 fixed bottom-0 sm:bottom-10 sm:right-10">
+            <Button
+              props={{
+                type: "button",
+                className: "text-red-500 shadow-md max-sm:w-full",
+                onClick: () => setIsConfirmCancelOpen(true),
+              }}
+            >
+              Cancelar
+            </Button>
+            <Button
+              props={{
+                type: "button",
+                className: "text-green-500 shadow-md max-sm:w-full",
+                onClick: () => setIsConfirmSaveOpen(true),
+              }}
+            >
+              Guardar alterações
+            </Button>
           </div>
-        ))}
-        <button
-          type="button"
-          onClick={() =>
-            appendPath({
-              pathTitle: "",
-              items: [
-                {
-                  id: null,
-                  pathId: null,
-                  content: "",
-                  date: "",
-                  title: "",
-                  titleLink: "",
-                  subTitle: "",
-                  subTitleLink: "",
-                },
-              ],
-            })
-          }
-          className="self-end border p-3 bg-white border-black text-green-500"
-        >
-          Novo percurso
-        </button>
-        <div className="flex justify-between max-sm:border-t max-sm:border-black max-sm:p-3 sm:justify-end gap-2 max-sm:bg-white w-full max-sm:left-0 fixed bottom-0 sm:bottom-10 sm:right-10">
-          <Button props={{ type: "button", className: "text-red-500 shadow-md max-sm:w-full", onClick: () => setIsViewMode(true) }}>
-            Cancelar
-          </Button>
-          <Button props={{ type: "submit", className: "text-green-500 shadow-md max-sm:w-full" }}>Guardar alterações</Button>
         </div>
-      </div>
-    </form>
+      </form>
+      <ConfirmationModal
+        text="Tens a certeza que desejas descartar as alterações?"
+        isOpen={isConfirmCancelOpen}
+        onClose={() => setIsConfirmCancelOpen(false)}
+        onConfirm={() => setIsViewMode(true)}
+      />
+      <ConfirmationModal
+        text="Tens a certeza que desejas guardar?"
+        isOpen={isConfirmSaveOpen}
+        onClose={() => setIsConfirmSaveOpen(false)}
+        onConfirm={() => {
+          setIsConfirmSaveOpen(false);
+          handleSubmit((data: PathFormSchemaType) => updatePath(data))();
+        }}
+      />
+    </>
   );
 };
 
